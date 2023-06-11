9a74dd6c7718d1f93f86849f03f4e3e712f692ee
emperorroars <daemosares@gmail.com.com>
2023-06-11 03:07:21 -0700

    commitnew

--- dom.js
+++ dom.js
@@ -9,4 +9,11 @@ var header=document.getElementById('main-header')
 header.style.borderBottom='solid 3px  #000'
 var addItemHeading = document.getElementById('main').getElementsByClassName('title')[0];
 addItemHeading.style.fontWeight = 'bold';
-addItemHeading.style.color = 'green';
+addItemHeading.style.color = 'pink';
+var listItems = document.getElementsByClassName('list-group-item');
+var thirdElement = listItems[2];
+thirdElement.style.backgroundColor ='green'
+var listItems = document.getElementsByClassName('list-group-item');
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontWeight = 'bold';
+}
