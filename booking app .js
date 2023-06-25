70fa0fa56477a9f25e68437c09d8cf1ecb49f65e
emperorroars <daemosares@gmail.com.com>
2023-06-25 01:46:37 -0700

    HI COMMIT

--- booking app .js
+++ booking app .js
@@ -1,23 +1,46 @@
 function submitForm(event) {
-    event.preventDefault();
-    var date = document.getElementById('date').value;
-    var email = document.getElementById('email').value;
-    var time = document.getElementById('time').value;
-    var phone = document.getElementById('phone').value;
-    var name = document.getElementById('name').value;
-    // Convert the userDetails object to a JSON string
+  event.preventDefault();
+
+  var date = document.getElementById('date').value;
+  var email = document.getElementById('email').value;
+  var time = document.getElementById('time').value;
+  var phone = document.getElementById('phone').value;
+  var name = document.getElementById('name').value;
+
+  // Create userDetails object
+  var userDetails = {
+    date: date,
+    email: email,
+    time: time,
+    phone: phone,
+    name: name
+  };
+
+  // Convert the userDetails object to a JSON string
   var userDetailsJSON = JSON.stringify(userDetails);
 
   // Store the userDetails JSON string in local storage
   localStorage.setItem('userDetails', userDetailsJSON);
 
-    console.log('Submitted Data:');
-    console.log('Date:', date);
-    console.log('Email:', email);
-    console.log('Time:', time);
-    console.log('Phone:', phone);
-    console.log('Name:', name);
-  }
+  console.log('Submitted Data:');
+  console.log('Date:', date);
+  console.log('Email:', email);
+  console.log('Time:', time);
+  console.log('Phone:', phone);
+  console.log('Name:', name);
+}
+var userDetailsJSON = localStorage.getItem('userDetails');
+
+// Parse the userDetails JSON string back to an object
+var userDetails = JSON.parse(userDetailsJSON);
+
+// Access and use the retrieved user details
+console.log('Retrieved Data:');
+console.log('Date:', userDetails.date);
+console.log('Email:', userDetails.email);
+console.log('Time:', userDetails.time);
+console.log('Phone:', userDetails.phone);
+console.log('Name:', userDetails.name);
 
 
 
