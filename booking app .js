0cc0569527131ba96e459a5d1881cd2c20ed4386
emperorroars <daemosares@gmail.com.com>
2023-06-27 03:46:46 -0700

    delete commit

--- booking app .js
+++ booking app .js
@@ -1,4 +1,3 @@
-// JavaScript code
 var userDetailsList = document.getElementById('userDetailsList');
 
 function submitForm(event) {
@@ -22,6 +21,21 @@ function submitForm(event) {
   // Add the userDetails object to the list
   addUserDetailsToList(userDetails);
 
+  // Convert the userDetails object to a JSON string
+  var userDetailsJSON = JSON.stringify(userDetails);
+
+  // Store the userDetails JSON string in local storage
+  var storedUserDetailsJSON = localStorage.getItem('userDetails');
+  var storedUserDetails = storedUserDetailsJSON ? JSON.parse(storedUserDetailsJSON) : [];
+
+  if (!Array.isArray(storedUserDetails)) {
+    // If the data is not an array, initialize an empty array
+    storedUserDetails = [];
+  }
+
+  storedUserDetails.push(userDetails);
+  localStorage.setItem('userDetails', JSON.stringify(storedUserDetails));
+
   console.log('Submitted Data:');
   console.log('Date:', date);
   console.log('Email:', email);
@@ -30,7 +44,19 @@ function submitForm(event) {
   console.log('Name:', name);
 
   // Reset the form
-  document.getElementById('myForm').reset();
+  //document.getElementById('myForm').reset();
+}
+
+var userDetailsJSON = localStorage.getItem('userDetails');
+
+if (userDetailsJSON) {
+  // Parse the userDetails JSON string back to an object
+  var storedUserDetails = JSON.parse(userDetailsJSON);
+
+  // Add the retrieved user details to the list
+  storedUserDetails.forEach(function(userDetails) {
+    addUserDetailsToList(userDetails);
+  });
 }
 
 function addUserDetailsToList(userDetails) {
@@ -39,11 +65,65 @@ function addUserDetailsToList(userDetails) {
 
   // Create paragraph element for displaying user details
   var detailsParagraph = document.createElement('p');
-  detailsParagraph.textContent = 'Name: ' + userDetails.name + ', Email: ' + userDetails.email + ', Phone: ' + userDetails.phone + ', Date: ' + userDetails.date + ', Time: ' + userDetails.time;
+  detailsParagraph.textContent =
+    'Name: ' +
+    userDetails.name +
+    ', Email: ' +
+    userDetails.email +
+    ', Phone: ' +
+    userDetails.phone +
+    ', Date: ' +
+    userDetails.date +
+    ', Time: ' +
+    userDetails.time;
 
-  // Append the details paragraph to the list item
+  // Create delete button element
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.addEventListener('click', function() {
+    // Handle delete button click event
+    deleteUserDetails(userDetails, listItem);
+  });
+
+  // Append the details paragraph and delete button to the list item
   listItem.appendChild(detailsParagraph);
+  listItem.appendChild(deleteButton);
 
   // Append the list item to the user details list
   userDetailsList.appendChild(listItem);
+}
+
+function deleteUserDetails(userDetails, listItem) {
+  // Retrieve the stored user details from local storage
+  var storedUserDetailsJSON = localStorage.getItem('userDetails');
+  if (storedUserDetailsJSON) {
+    var storedUserDetails = JSON.parse(storedUserDetailsJSON);
+
+    // Find the index of the user details to be deleted
+    var index = -1;
+    for (var i = 0; i < storedUserDetails.length; i++) {
+      var item = storedUserDetails[i];
+      if (
+        item.name === userDetails.name &&
+        item.email === userDetails.email &&
+        item.phone === userDetails.phone &&
+        item.date === userDetails.date &&
+        item.time === userDetails.time
+      ) {
+        index = i;
+        break;
+      }
+    }
+
+    if (index !== -1) {
+      // Remove the user details from the stored array
+      storedUserDetails.splice(index, 1);
+
+      // Update the local storage with the updated JSON string
+      localStorage.setItem('userDetails', JSON.stringify(storedUserDetails));
+
+      // Remove the list item from the user details list
+      listItem.parentNode.removeChild(listItem);
+    }
+  }
 }
