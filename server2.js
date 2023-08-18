e087d1ea9f06929259ad1d594606cc8dbf179e3e
emperorroars <daemosares@gmail.com.com>
2023-08-17 21:21:50 -0700

    parsing

--- server2.js
+++ server2.js
@@ -1,16 +1,48 @@
 const http = require('http');
+const fs=require('fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-type', 'text/html');
+   // console.log(req.url, req.method, req.headers);
+   const url=req.url
+   const method=req.method
+    
 
-    if (req.url === '/home') {
+    if (url === '/') {
+     
         res.write('<html>');
-        res.write('<head><title>Home Page</title></head>');
-        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('<head><title>Enter message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        res.end();
-    } else if (req.url === '/about') {
+       return res.end();
+    }
+    if(url === '/message' && method==='POST')
+    {
+        const body=[]
+        req.on('data',(chunk)=>{
+            console.log(chunk)
+            body.push(chunk)         
+        })
+        req.on('end',()=>{
+           const parsedBody=Buffer.concat(body).toString()
+           const message=parsedBody.split('=')[1]   
+           console.log(message)
+           fs.writeFileSync('message.txt', message)   
+          
+        })
+      
+        res.statusCode=302
+           res.setHeader('Location','/')
+           return res.end()
+     
+
+    }
+   
+   // res.setHeader('Content-type','text/html')
+   /* res.write('<html>')
+    res.write('<head><title>My first page</title></head>')
+    res.write('<body><h1>Hello from my Node.js Server</h1></body>')
+    res.write('</html>')
+    res.end() else if (req.url === '/about') {
         res.write('<html>');
         res.write('<head><title>About Us</title></head>');
         res.write('<body><h1>Welcome to About Us page</h1></body>');
@@ -29,6 +61,7 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         res.end();
     }
+    */
 });
 
 server.listen(4000);
