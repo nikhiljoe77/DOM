6ebb182bfa1ae9856764e983be10d2922a31c189
emperorroars <daemosares@gmail.com.com>
2023-08-28 11:25:53 -0700

    filter

--- server2.js
+++ server2.js
@@ -1,30 +1,19 @@
 const http = require('http');
 const express=require("express")
+const router=express.Router()
 const bodyParser=require('body-parser')
 const app=express()
+const adminRoutes=require('./routes/admin.JS')
+const shopRoutes=require('./routes/shop.JS')
 
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/admin',adminRoutes)
+app.use('/shop',shopRoutes)
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 
-/*app.use('/',(req,res,next)=>{
-    console.log("This always runs")
-    next()
-  
-})*/
-app.use('/add-product',(req,res,next)=>{
-    
-    res.send('<form action="/product"method="POST"><input type="text"name="title"><button type="submit">Add Product</button><label for="size">Product Size:</label> <input type="text" id="size" name="size"></form>')
-  
 })
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    
-    res.send('<h1>Hello from Express</h1>')
-  
-})
 
 
 
