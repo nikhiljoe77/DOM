69a76f79aa04bf2315a5d1730353b17784453c65
emperorroars <daemosares@gmail.com.com>
2023-09-10 04:34:54 -0700

    controller

---
+++ 00-starting-setup/routes/shop.js
@@ -0,0 +1,9 @@
+
+const path=require("path")
+const express = require('express');
+const productsController=require('../controllers/products')
+const router = express.Router();
+
+router.get('/', productsController.getProduct);
+
+module.exports = router;
