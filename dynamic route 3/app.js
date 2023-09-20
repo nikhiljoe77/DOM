3d0a410966b60f869f6be57608f1026a0236792e
emperorroars <daemosares@gmail.com.com>
2023-09-20 08:40:54 -0700

    SQL RETRIEVING DATA

--- dynamic route 3/app.js
+++ dynamic route 3/app.js
@@ -13,7 +13,6 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-db.execute('SWLECT*FROM products')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
